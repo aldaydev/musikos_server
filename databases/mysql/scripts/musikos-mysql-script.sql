ccc570ce47f5ebeb14a7c3ca192cf2f15716e5ce
aldaydev <rafaldayparejo@gmail.com>
2025-02-27 05:18:02 +0100

    adding mysql script

---
+++ databases/mysql/scripts/musikos-mysql-script.sql
@@ -0,0 +1,14 @@
+DROP DATABASE musikos;
+CREATE DATABASE musikos;
+
+USE musikos;
+SELECT * FROM musicians;
+SELECT * FROM styles;
+SELECT * FROM instruments;
+SELECT * FROM towns;
+SELECT * FROM provinces;
+SELECT * FROM regions;
+
+SELECT COUNT(*) FROM towns;
+SELECT COUNT(*) FROM provinces;
+SELECT COUNT(*) FROM regions;
